79c62219931bfbc732f990b81f6fc969c147e687
ZeioNara <zeionara@gmail.com>
2016-10-23 15:38:52 +0300

    Created simple UI (without graph and action listeners)

---
+++ src/jswing/GraphPanel.java
@@ -0,0 +1,13 @@
+package jswing;
+
+import javax.swing.*;
+import java.awt.*;
+
+/**
+ * Created by Zerbs on 23.10.2016.
+ */
+public class Graph extends JPanel {
+    public void paint(Graphics g){
+        g.drawLine(20, 20, 100, 20);
+    }
+}
